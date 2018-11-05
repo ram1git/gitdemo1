09d1ad043e5edd268904188ae84030b9e33de859
ananth <pananthram.1972@gmail.com>
2018-11-05 11:46:44 +0530

    adding to source

--- src/test/java/gitdemo1/SomeTest.java
+++ src/test/java/gitdemo1/SomeTest.java
@@ -4,8 +4,9 @@ public class SomeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int a=9;
 System.out.println("hello");
+System.out.println("text");
 	}
 
 }
